60ff6966ad9779f42c43f0659c458743576c6309
matthewb22 <matthew@take2nz.org>
2022-12-20 20:16:23 +1300

    Add a Take2 Logo

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import rocketseatLogo from './assets/take2_logo.png';
+
+const Logo = () => (
+  <img src={rocketseatLogo} alt="Rocketseat logo" style={{ width: 180 }} />
+);
+
+console.log('Loading Take2 Logo');
+
+export default Logo;
